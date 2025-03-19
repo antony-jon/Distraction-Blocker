3f5febf3f9e1b7afb836de475ddb494e6ec1193e
antony-jon <antony23452625@gmail.com>
2025-03-19 15:36:36 +0530

    fix : removed extras

--- backend_db/models/BlockedURL.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const BlockedURLSchema = new mongoose.Schema({
-    adminId: { type: String, required: true },
-    url: { type: String, required: true },
-});
-
-module.exports = mongoose.model("BlockedURL", BlockedURLSchema);
