f431de9195c1a62c48265e82e32ccd561615beb1
Girinath04 <girinathdurai@gmail.com>
2025-03-20 11:24:18 +0530

    added : little more

---
+++ parentSignIn.js
@@ -0,0 +1,33 @@
+document.getElementById("signInForm").addEventListener("submit", async function (event) {
+    event.preventDefault(); 
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const errorMessage = document.getElementById("errorMessage");
+
+    try {
+        const response = await fetch("http://localhost:5000/api/parent/signin", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ email, password }),
+        });
+
+        const data = await response.json();
+
+        if (data.success) {
+            localStorage.setItem("userType", "Parent");
+            localStorage.setItem("userEmail", data.email);
+            alert("Sign in successful!");
+            window.location.href = ""; ///////////////////////////////////////////////////////////WAIT fixing
+        } else {
+            errorMessage.textContent = data.message;
+            errorMessage.style.display = "block";
+        }
+    } catch (error) {
+        console.error("Error:", error);
+        errorMessage.textContent = "Server error. Try again later.";
+        errorMessage.style.display = "block";
+    }
+});
